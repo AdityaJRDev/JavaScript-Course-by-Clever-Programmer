73c1159a71f9a048552653ba72d9edb7ce8d0801
cleverprogrammer <>
2025-04-17 10:21:38 +0530

    Completed DOM exercise

--- dom-exercise/script.js
+++ dom-exercise/script.js
@@ -1,7 +1,23 @@
-let redDiv = document.getElementById('red');
-let yellowDiv = document.getElementById('yellow');
-let greenDiv = document.getElementById('green');
+const squares = document.querySelectorAll('.colorSquare')
 
-redDiv.onclick = () => console.log('User choice: Rock')
-yellowDiv.onclick = () => console.log('User choice: Paper')
-greenDiv.onclick = () => console.log('User choice: Scissor')
+// forEach
+const timesClicked = {'red': 0, "yellow": 0, "green": 0}
+squares.forEach(square => {
+    square.onclick = () => {
+        timesClicked[square.value] += 1
+        square.innerText = timesClicked[square.value]
+        // console.log(square.value)
+    }
+})
+
+function clearScores() {
+    timesClicked.red = 0
+    timesClicked.yellow = 0
+    timesClicked.green = 0
+    squares.forEach(sqaure => {
+        sqaure.innerText = ''
+    })
+}
+
+const clearGameBtn = document.getElementById('clear-game')
+clearGameBtn.onclick = () => clearScores()
